f029ee23b6476e95f4b44074321a3f04c65b7936
Yuliya <yulua03071992@gmail.com>
2022-04-19 16:07:15 +0300

    change arrows in slider

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,6 @@ var swiper = new Swiper('.mySwiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  pagination: {
-    el: '.swiper-pagination',
-  },
   mousewheel: true,
   keyboard: true,
 });
